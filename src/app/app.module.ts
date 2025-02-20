06ba8d6532f6121c001ac5607550513b6390c3b2
anil <anil2anil@gmail.com>
2025-02-21 01:34:33 +0530

    Add page footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { NavbarMainComponent } from './pages/navbar-main/navbar-main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    NavbarMainComponent
+    NavbarMainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
