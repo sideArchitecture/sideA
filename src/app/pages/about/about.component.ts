8abc8cd5d3f5165d76048122d52e96215d82b6e4
anil <anil2anil@gmail.com>
2025-09-24 16:55:42 +0530

    Move people to about us

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {gsap} from "gsap";
 
 @Component({
   selector: 'app-about',
@@ -11,5 +12,33 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  ngAfterViewInit() {
+    gsap.from('.principal-profile', {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      ease: 'power2.out'
+    });
+  }
+
+
+
+  copied: boolean = false;
+
+  copyEmail(email: string): void {
+    navigator.clipboard.writeText(email).then(() => {
+      this.copied = true;
+
+      // Hide message after 2 seconds
+      setTimeout(() => {
+        this.copied = false;
+      }, 2000);
+    }).catch(err => {
+      console.error('Failed to copy email:', err);
+    });
+  }
+
 
 }
