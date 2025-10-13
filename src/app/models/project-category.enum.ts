ffa9a04087bf8e753eddd437bd9bc6f313b594b0
anil <anil2anil@gmail.com>
2025-10-13 15:51:26 +0530

    Update project categories
    Add new project categories and reorder

--- src/app/models/project-category.enum.ts
+++ src/app/models/project-category.enum.ts
@@ -1,10 +1,30 @@
 // src/app/models/project-category.enum.ts
 export enum ProjectCategory {
+  Featured = 'featured',
   Residential = 'residential',
-  Commercial = 'commercial',
+  Office = 'office',
   Institutional = 'institutional',
-  Cultural = 'cultural',
   Interior = 'interior',
-  Landscape = 'landscape',
-  Featured = 'featured'
+  Hospitality = 'hospitality',
+  Branding = 'branding',
+  HealthCare = 'healthcare',
+  Commercial = 'commercial',
 }
+
+/*
+
+
+// src/app/models/project-category.enum.ts
+export enum ProjectCategory {
+  01 Featured = 'featured',
+  02 Residential = 'residential',
+  03 Office = 'office',
+  04 Institutional = 'institutional',
+  05 Interior = 'interior',
+  06 Hospitality = 'hospitality',
+  07 Branding = 'branding',
+  08 HealthCare = 'healthcare',
+  09 Commercial = 'commercial',
+}
+
+*/
