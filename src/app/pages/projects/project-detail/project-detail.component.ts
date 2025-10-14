030f383c911f497217aec8b8278557542422481d
anil <anil2anil@gmail.com>
2025-10-14 18:34:22 +0530

    Fetch images in local for project detail

--- src/app/pages/projects/project-detail/project-detail.component.ts
+++ src/app/pages/projects/project-detail/project-detail.component.ts
@@ -4,6 +4,8 @@ import { ProjectService } from '../../../services/project.service';
 import { Project } from '../../../models/project.model';
 import { gsap } from 'gsap';
 import {Title} from "@angular/platform-browser";
+import {environment} from "../../../../environments/environment";
+
 
 @Component({
   selector: 'app-project-detail',
@@ -57,6 +59,15 @@ export class ProjectDetailComponent implements OnInit {
     });
   }
 
+  getCoverImageUrl(url: string): string {
+    const isDev = !environment.production;
+    const localBase = 'http://localhost:8081/images/projects';
+    return isDev
+      ? url.replace('https://sidearchitecture.github.io/sideAImages/images/projects', localBase)
+      : url;
+  }
+
+
   loadValidImages(): void {
     if (!this.project?.imageUrls?.length) return;
 
@@ -64,26 +75,35 @@ export class ProjectDetailComponent implements OnInit {
     const valid: (string | null)[] = new Array(imageCount).fill(null);
     let loadedCount = 0;
 
+    const isDev = !environment.production;
+    const localBase = 'http://localhost:8081/projects';
+
     this.project.imageUrls.forEach((path, index) => {
-      const img = new Image();
-      img.onload = () => {
-        valid[index] = path;
-        loadedCount++;
-        if (loadedCount === imageCount) {
-          this.validImages = valid.filter(Boolean) as string[];
-          this.animateImages();
-        }
-      };
-      img.onerror = () => {
-        loadedCount++;
-        if (loadedCount === imageCount) {
-          this.validImages = valid.filter(Boolean) as string[];
-          this.animateImages();
-        }
-      };
-      img.src = path;
-    });
+        const rewrittenPath = isDev
+          ? path.replace('https://sidearchitecture.github.io/sideAImages/images/projects', localBase)
+          : path;
+
+        const img = new Image();
+        img.onload = () => {
+          valid[index] = rewrittenPath;
+          loadedCount++;
+          if (loadedCount === imageCount) {
+            this.validImages = valid.filter(Boolean) as string[];
+            this.animateImages();
+          }
+        };
+        img.onerror = () => {
+          loadedCount++;
+          if (loadedCount === imageCount) {
+            this.validImages = valid.filter(Boolean) as string[];
+            this.animateImages();
+          }
+        };
+        img.src = rewrittenPath;
+      }
+    );
   }
+
 
   private animateImages(): void {
     setTimeout(() => {
