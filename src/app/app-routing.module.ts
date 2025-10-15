eef887c902c0b1092c4e7a955abf4994f2d47f61
anil <anil2anil@gmail.com>
2025-10-15 09:12:19 +0530

    Remove the hash routing in the url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true ,
+    useHash: false ,
 
 
     // scrollPositionRestoration: 'enabled', // ✅ restores scroll on back/forward
