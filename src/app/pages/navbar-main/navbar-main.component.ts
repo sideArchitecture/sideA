d332970be6468ee2a2fdbe3057f44a01e1b90275
anil <anil2anil@gmail.com>
2026-06-03 17:51:15 +0530

    Highlight selected route

--- src/app/pages/navbar-main/navbar-main.component.ts
+++ src/app/pages/navbar-main/navbar-main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-navbar-main',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-main.component.scss']
 })
 export class NavbarMainComponent {
+
+  constructor(public router: Router) {}
 
   toggleNavbar() {
     const navbarCollapse = document.getElementById('navbarNav');
